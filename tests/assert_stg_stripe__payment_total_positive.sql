266714fa4a28f8001297d839be5c82c7f54d7664
Nelson Mak <nelsonkmak@gmail.com>
2026-06-04 15:30:35 +0000

    add descriptions

--- tests/assert_stg_stripe__payment_total_positive.sql
+++ tests/assert_stg_stripe__payment_total_positive.sql
@@ -1,9 +1,8 @@
-select 
-    order_id,
-    sum(payment_amount) as total_amount
-from
-    {{ ref('stg_stripe__payments') }}
+-- Refunds have a negative amount, so the total amount should always be >= 0.
+-- Therefore return records where this isn't true to make the test fail.
+select
+  order_id,
+  sum(amount) as total_amount
+from {{ ref('stg_stripe__payment') }}
 group by 1
--- cannot have semicolon for tests
--- having filter is before SELECT clause, so we have to use sum(payment_amount)
-having sum(payment_amount) < 0 
+having (total_amount < 0)
